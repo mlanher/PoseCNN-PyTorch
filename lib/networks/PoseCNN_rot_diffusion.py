0c7780c467628e57a68d4376dcce18637be3fcfe
mlanher <mlanher@users.noreply.github.com>
2023-08-20 23:17:45 +0200

    Introduce adaptive tanh activation function which ensures noisy pose targets in range (-1, 1) but still preserves the correlation between the added noise and the corresponding noise scale

--- lib/networks/PoseCNN_rot_diffusion.py
+++ lib/networks/PoseCNN_rot_diffusion.py
@@ -159,7 +159,7 @@ class RotationDiffusion(nn.Module):
 
         self.attention = nn.Sequential(
             nn.Linear(1024, 1024),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.Linear(1024, 1),
             nn.Softmax(dim=1)
         )
@@ -167,18 +167,24 @@ class RotationDiffusion(nn.Module):
         self.fc1 = nn.Sequential(
             nn.Linear(1024 + 88 + 1, 512),  # 1024 from feature map, 88 from target pose, 1 from noise scale
             nn.BatchNorm1d(512),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Dropout(0.5)
         )
 
         self.fc2 = nn.Sequential(
             nn.Linear(512, 256),
             nn.BatchNorm1d(256),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Dropout(0.5)
         )
 
         self.fc3 = nn.Linear(256, 4 * num_classes)
+
+    def adaptive_tanh(self, x: torch.Tensor, noise_scale: torch.Tensor):
+        """
+        Ensures range (-1, 1) while preserving the correlation of the added noise and the noise scale
+        """
+        return torch.tanh(x * noise_scale)
 
     def forward(self, roi_pool, noisy_poses_target, noise_scale):
         roi_pool_reduced = self.fc_dim_reduction(roi_pool)
@@ -186,7 +192,8 @@ class RotationDiffusion(nn.Module):
         attention_weights = self.attention(roi_pool_reduced)
         roi_pool_attention = attention_weights * roi_pool_reduced
 
-        out = torch.cat((roi_pool_attention, noisy_poses_target, noise_scale), dim=1)
+        noisy_poses_ensured_range = self.adaptive_tanh(noisy_poses_target, noise_scale)
+        out = torch.cat((roi_pool_attention, noisy_poses_ensured_range, noise_scale), dim=1)
         out = self.fc1(out)
         out = self.fc2(out)
         out = self.fc3(out)
