cffb6067aaad294b370d8e3c897d2f3cfccea515
mlanher <mlanher@users.noreply.github.com>
2023-08-23 20:26:29 +0200

    refactor: Move argument parser for experiments in test context to own file. Will be used for multiple test implementations

---
+++ tools/test_argument_parser.py
@@ -0,0 +1,38 @@
+import argparse
+import sys
+
+
+def parse_args_test():
+    """
+    Parse input arguments when performing tests.
+    """
+    parser = argparse.ArgumentParser(description='Test a network')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU id to use',
+                        default=0, type=int)
+    parser.add_argument('--pretrained', dest='pretrained',
+                        help='initialize with pretrained checkpoint',
+                        default=None, type=str)
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file', default=None, type=str)
+    parser.add_argument('--dataset', dest='dataset_name',
+                        help='dataset to train on',
+                        default='shapenet_scene_train', type=str)
+    parser.add_argument('--dataset_background', dest='dataset_background_name',
+                        help='background dataset to train on',
+                        default='background_nvidia', type=str)
+    parser.add_argument('--rand', dest='randomize',
+                        help='randomize (do not use a fixed seed)',
+                        action='store_true')
+    parser.add_argument('--network', dest='network_name',
+                        help='name of the network',
+                        default=None, type=str)
+    parser.add_argument('--background', dest='background_name',
+                        help='name of the background file',
+                        default=None, type=str)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    return args
--- tools/test_net.py
+++ tools/test_net.py
@@ -24,46 +24,14 @@ from fcn.test_dataset import test
 from fcn.config import cfg, cfg_from_file, get_output_dir
 from datasets.factory import get_dataset
 import networks
+
+from test_argument_parser import parse_args_test
 from ycb_renderer import YCBRenderer
 from sdf.sdf_optimizer import sdf_optimizer
 
-def parse_args():
-    """
-    Parse input arguments
-    """
-    parser = argparse.ArgumentParser(description='Test a PoseCNN network')
-    parser.add_argument('--gpu', dest='gpu_id', help='GPU id to use',
-                        default=0, type=int)
-    parser.add_argument('--pretrained', dest='pretrained',
-                        help='initialize with pretrained checkpoint',
-                        default=None, type=str)
-    parser.add_argument('--cfg', dest='cfg_file',
-                        help='optional config file', default=None, type=str)
-    parser.add_argument('--dataset', dest='dataset_name',
-                        help='dataset to train on',
-                        default='shapenet_scene_train', type=str)
-    parser.add_argument('--dataset_background', dest='dataset_background_name',
-                        help='background dataset to train on',
-                        default='background_nvidia', type=str)
-    parser.add_argument('--rand', dest='randomize',
-                        help='randomize (do not use a fixed seed)',
-                        action='store_true')
-    parser.add_argument('--network', dest='network_name',
-                        help='name of the network',
-                        default=None, type=str)
-    parser.add_argument('--background', dest='background_name',
-                        help='name of the background file',
-                        default=None, type=str)
-
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
-
-    args = parser.parse_args()
-    return args
 
 if __name__ == '__main__':
-    args = parse_args()
+    args = parse_args_test()
 
     print('Called with args:')
     print(args)
